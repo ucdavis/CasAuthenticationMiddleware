28ad4ed3bd7cdaa8778d443a829f133426ce421d
Scott Kirkland <srkirkland@gmail.com>
2015-12-08 10:53:13 -0800

    Handle returnURL and also grab ticket and query

--- src/CasAuthenticationMiddleware/CasAuthenticationHandler.cs
+++ src/CasAuthenticationMiddleware/CasAuthenticationHandler.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Authentication;
@@ -8,22 +10,48 @@ namespace CasAuthenticationMiddleware
 {
     public class CasAuthenticationHandler<TOptions> : RemoteAuthenticationHandler<TOptions> where TOptions : CasAuthenticationOptions
     {
+        private const string StrTicket = "ticket";
+        private const string StrReturnUrl = "ReturnURL";
+
         protected override async Task<AuthenticateResult> HandleRemoteAuthenticateAsync()
         {
-            var query = Context.Request.Query;
-            var identity = new ClaimsIdentity(Options.ClaimsIssuer);
-            identity.AddClaim(new Claim(ClaimTypes.Name, "postit", ClaimValueTypes.String, Options.ClaimsIssuer));
-            var principal = new ClaimsPrincipal(identity);
+            // build query string but strip out ticket if it is defined
+            var query = Context.Request.Query.Keys.Where(
+                key => string.Compare(key, StrTicket, StringComparison.OrdinalIgnoreCase) != 0)
+                .Aggregate("", (current, key) => current + ("&" + key + "=" + Context.Request.Query[key]));
 
-            var authTicket = new AuthenticationTicket(principal, new AuthenticationProperties(), "CAS");
+            // replace 1st character with ? if query is not empty
+            if (!string.IsNullOrEmpty(query))
+            {
+                query = "?" + query.Substring(1);
+            }
 
-            var uri = CurrentUri;
-            return await Task.FromResult(AuthenticateResult.Success(authTicket));
+            // get ticket & service
+            string ticket = Context.Request.Query[StrTicket];
+            var returnUrl = Context.Request.Query[StrReturnUrl];
+            string service = Context.Request.Path.ToUriComponent() + query;
+
+            if (string.IsNullOrWhiteSpace(ticket))
+            {
+                return await Task.FromResult(AuthenticateResult.Failed("No authorization ticket found"));
+            }
+            else
+            {
+                //todo: backchannel call
+                var identity = new ClaimsIdentity(Options.ClaimsIssuer);
+                identity.AddClaim(new Claim(ClaimTypes.Name, "postit", ClaimValueTypes.String, Options.ClaimsIssuer));
+                var principal = new ClaimsPrincipal(identity);
+
+                var authTicket = new AuthenticationTicket(principal,
+                    new AuthenticationProperties {RedirectUri = returnUrl}, "CAS");
+
+                return await Task.FromResult(AuthenticateResult.Success(authTicket));
+            }
         }
 
         protected override async Task<bool> HandleUnauthorizedAsync(ChallengeContext context)
         {
-            var authorizationEndpoint = Options.AuthorizationEndpoint + "login?service=" + BuildRedirectUri(Options.CallbackPath);
+            var authorizationEndpoint = Options.AuthorizationEndpoint + "login?service=" + BuildRedirectUri(Options.CallbackPath) + "?" + StrReturnUrl + "=" + Context.Request.Path;
 
             Context.Response.Redirect(authorizationEndpoint);
 
